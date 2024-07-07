2581d1a8e83517a1b5f13e03f77317bc4c08c8bb
dvub <dvubdevs@gmail.com>
2024-07-07 01:04:54 -0700

    just foolin around

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use cliclack::{input, intro, note};
-use std::{
-    env::current_dir,
-    fs::{copy, create_dir_all, read_dir, File},
-    io::Write,
-    path::Path,
-    process::Command,
-};
-
+use nih_plug_xtask::bundle;
+use nih_plug_xtask::{build, chdir_workspace_root};
+use std::{env::current_dir, fs::File, io::Write, process::Command};
 /// NIH-Plug CLI.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
+struct Cli {
     #[command(subcommand)]
     command: Commands,
+
+    #[arg(trailing_var_arg = true)]
+    args: Vec<String>,
 }
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// Create a new NIH-Plug project (interactive)
     New,
     /// Compile an existing NIH-Plug project
-    Build { name: String },
+    Bundle {
+        packages: Vec<String>,
+    },
+
+    BundleUniversal {
+        packages: Vec<String>,
+    },
 }
 
 #[derive(boilerplate::Boilerplate)]
@@ -35,14 +38,26 @@ struct LibTxt {
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
+    let args = Cli::parse();
+    println!("{:?}", args.args);
+
+    chdir_workspace_root()?;
+    let cargo_metadata = cargo_metadata::MetadataCommand::new()
+        .manifest_path("./Cargo.toml")
+        .exec()
+        .unwrap(); // TODO: dont unwrap
+    let target_dir = cargo_metadata.target_directory.as_std_path();
 
     match args.command {
-        Commands::Build { name } => {
-            compile(name);
+        Commands::Bundle { packages } => {
+            build(&packages, &[String::new()])?;
+            bundle(target_dir, &packages[0], &[String::new()], false)?;
+            for package in packages.into_iter().skip(1) {
+                bundle(target_dir, &package, &[String::new()], false)?;
+            }
             Ok(())
         }
-        Commands::New => create_project(),
+        _ => todo!(),
     }
 }
 
@@ -128,8 +143,6 @@ fn create_project() -> Result<()> {
      * With Cargo.toml, we need to do some things
      * 1. add nih-plug using the git link
      * 2. indicate that this project is a cdylib
-     * 3. indicate that this will be a workspace,
-     *    and that this workspace will have a new member called xtask (for compilation)
      */
 
     let mut file = File::options()
@@ -137,13 +150,8 @@ fn create_project() -> Result<()> {
         .open(project_path.join("Cargo.toml"))
         .unwrap();
 
-    writeln!(file, "nih_plug = {{ git = \"https://github.com/robbert-vdh/nih-plug.git\", features = [\"assert_process_allocs\"] }}\n\n[lib]\ncrate-type = [\"cdylib\"]\n\n[workspace]\nmembers=[\"xtask\"]")
+    writeln!(file, "nih_plug = {{ git = \"https://github.com/robbert-vdh/nih-plug.git\", features = [\"assert_process_allocs\"] }}\n\n[lib]\ncrate-type = [\"cdylib\"]\n")
     .unwrap();
-
-    // xtask
-    create_xtask(&project_path);
-
-    // if there's no existing .gitignore, create a new one
 
     let gitignore_path = project_path.join(".gitignore");
     let mut gitignore = File::create(gitignore_path).unwrap();
@@ -167,40 +175,5 @@ fn create_project() -> Result<()> {
     lib.write_all(output.as_bytes())
         .expect("Error writing file");
 
-    Ok(())
-}
-
-fn create_xtask(path: &Path) {
-    copy_dir_all(
-        "D:\\projects\\rust\\npcli\\templates\\xtask",
-        path.join("xtask"),
-    )
-    .unwrap();
-}
-
-// extremely rare good SO answer
-// https://stackoverflow.com/questions/26958489/how-to-copy-a-folder-recursively-in-rust
-
-fn copy_dir_all(source_dir: impl AsRef<Path>, destination_dir: impl AsRef<Path>) -> Result<()> {
-    // this dir probably wont exist, but what if...
-    create_dir_all(&destination_dir)?;
-    println!("ASHDASKD");
-    for entry in read_dir(source_dir)? {
-        let entry = entry?;
-        // if we're reading a directory...
-        if entry.file_type()?.is_dir() {
-            // recursive function call
-            copy_dir_all(
-                entry.path(),
-                destination_dir.as_ref().join(entry.file_name()),
-            )?;
-        } else {
-            // if we're reading a file, just copy it
-            copy(
-                entry.path(),
-                destination_dir.as_ref().join(entry.file_name()),
-            )?;
-        }
-    }
     Ok(())
 }
