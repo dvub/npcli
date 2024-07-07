7655d5f652b339d26def9613a71ab6b9749fd313
dvub <dvubdevs@gmail.com>
2024-07-07 02:29:14 -0700

    progress :3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use cliclack::{input, intro, note};
+use cliclack::{input, intro, multiselect, note, select};
 use nih_plug_xtask::bundle;
 use nih_plug_xtask::{build, chdir_workspace_root};
 use std::{env::current_dir, fs::File, io::Write, process::Command};
@@ -10,21 +10,24 @@ use std::{env::current_dir, fs::File, io::Write, process::Command};
 struct Cli {
     #[command(subcommand)]
     command: Commands,
-
-    #[arg(trailing_var_arg = true)]
-    args: Vec<String>,
 }
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Create a new NIH-Plug project (interactive)
+    /// Create a new NIH-Plug project through a step-by-step, interactive CLI.
     New,
     /// Compile an existing NIH-Plug project
     Bundle {
+        /// Package(s) to compile.
         packages: Vec<String>,
+
+        /// Any other arguments supported by cargo, such as profile arguments (`--release`), may be supplied here.
+        /// To pass these arguments, you must first include `--`. E.g. `-- --release --profile ...`
+        #[arg(raw = true)]
+        other_args: Vec<String>,
     },
 
     BundleUniversal {
-        packages: Vec<String>,
+        packages: String,
     },
 }
 
@@ -35,46 +38,37 @@ struct LibTxt {
     url: String,
     email: String,
     vst_id: String,
+    midi_config: String,
+    sub_categories: String,
 }
 
 fn main() -> Result<()> {
-    let args = Cli::parse();
-    println!("{:?}", args.args);
-
-    chdir_workspace_root()?;
+    // chdir_workspace_root()?;
     let cargo_metadata = cargo_metadata::MetadataCommand::new()
         .manifest_path("./Cargo.toml")
         .exec()
-        .unwrap(); // TODO: dont unwrap
+        .unwrap(); // TODO
     let target_dir = cargo_metadata.target_directory.as_std_path();
 
+    let args = Cli::parse();
     match args.command {
-        Commands::Bundle { packages } => {
-            build(&packages, &[String::new()])?;
-            bundle(target_dir, &packages[0], &[String::new()], false)?;
+        Commands::New => create_project()?,
+
+        Commands::Bundle {
+            packages,
+            other_args,
+        } => {
+            build(&packages, &other_args)?;
+
+            bundle(target_dir, &packages[0], &other_args, false)?;
             for package in packages.into_iter().skip(1) {
-                bundle(target_dir, &package, &[String::new()], false)?;
+                bundle(target_dir, &package, &other_args, false)?;
             }
-            Ok(())
         }
         _ => todo!(),
-    }
-}
+    };
 
-fn compile(name: String) {
-    let output = Command::new("cmd")
-        .args([
-            "/C",
-            &format!(
-                "cargo run --package xtask --release -- bundle {} --release",
-                name
-            ),
-        ])
-        .output()
-        .expect("failed to execute process");
-
-    println!("{:?}", String::from_utf8(output.stdout).unwrap());
-    println!("{:?}", String::from_utf8(output.stderr).unwrap());
+    Ok(())
 }
 
 fn create_project() -> Result<()> {
@@ -114,7 +108,7 @@ fn create_project() -> Result<()> {
         .default_input(default_email)
         .interact()
         .unwrap();
-    let vst_id: String = input("VST ID")
+    let vst_id: String = input("VST ID:")
         .placeholder(default_vst_id)
         .default_input(default_vst_id)
         .validate(|input: &String| {
@@ -126,18 +120,71 @@ fn create_project() -> Result<()> {
         })
         .interact()
         .unwrap();
+    let midi_config: String = select("MIDI Config:")
+        .item("None", "None", "The plugin will not receive MIDI events.")
+        .item("Basic", "Basic", "The plugin receives note on/off/choke events, pressure, and possibly standardized expression types.")
+        .item(
+            "MidiCC",
+            "Full",
+            "The plugin receives full MIDI CCs as well as pitch bend information.",
+        )
+        .interact()
+        .unwrap()
+        .to_owned();
+    let main_vst_sub_category = select("Main VST category:")
+        .item("Fx", "Fx", "")
+        .item("Instrument", "Instrument", "")
+        .item("Spatial", "Spatial", "")
+        .interact()
+        .unwrap();
+    let other_sub_categories = [
+        "Analyzer",
+        "Delay",
+        "Distortion",
+        "Drum",
+        "Dynamics",
+        "Eq",
+        "External",
+        "Filter",
+        "Generator",
+        "Mastering",
+        "Modulation",
+        "Network",
+        "Piano",
+        "PitchShift",
+        "Restoration",
+        "Reverb",
+        "Sampler",
+        "Synth",
+        "Tools",
+        "UpDownmix",
+    ];
+    let mut multi = multiselect("Other VST categories:");
+    for cat in other_sub_categories {
+        multi = multi.item(cat, cat, "");
+    }
+    multi.interact().unwrap();
 
     let current_dir = current_dir().unwrap();
     let project_path = current_dir.join(&project_name);
 
     // create a new project with cargo
     // TODO: make sure user has cargo installed
+    let command = format!("cargo new --lib {}", project_name);
 
-    Command::new("cmd")
-        .args(["/C", &format!("cargo new --lib {}", project_name)])
-        .current_dir(&current_dir)
-        .output()
-        .expect("failed to execute process");
+    // curse you windows!
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", &command])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(&command)
+            .output()
+            .expect("failed to execute process")
+    };
 
     /*
      * With Cargo.toml, we need to do some things
@@ -170,8 +217,11 @@ fn create_project() -> Result<()> {
         url,
         email,
         vst_id,
+        midi_config,
+        sub_categories: "".to_string(),
     }
     .to_string();
+
     lib.write_all(output.as_bytes())
         .expect("Error writing file");
 
